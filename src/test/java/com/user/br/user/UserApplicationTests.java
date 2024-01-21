4f4db0913cc91773ab98ed41daf3cf7ded9cda39
Lucas Pereira <lucaspereira@MacBook-Air-de-Lucas.local>
2024-01-21 13:38:38 -0300

    iniciando projeto decoder

---
+++ src/test/java/com/user/br/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.br.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.isTrue(true, "Passou com sucesso");
+	}
+
+}
