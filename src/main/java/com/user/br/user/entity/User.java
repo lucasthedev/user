d5aad02fccd4188c9a0af0e1794c461a26888404
lucasthedev <lucas_wuthang@hotmail.com>
2024-03-20 16:48:15 -0300

    ajustes

--- src/main/java/com/user/br/user/entity/User.java
+++ src/main/java/com/user/br/user/entity/User.java
@@ -48,9 +48,9 @@ public class User extends RepresentationModel<User> implements Serializable {
     @Column
     private String imageUrl;
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime lastUpdateDate;
 }
